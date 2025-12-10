a02cecbc3d74670d6037b9de97b6a96c88f7ee71
mohamed <mohammad.bushra@mensagroup.net>
2025-12-10 10:23:58 +0400

    Add Building Blocs

---
+++ src/BuildingBlocs/BuildingBlocs/CQRS/ICommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace BuildingBlocs.CQRS
+{
+    //Guid represent the Guid type of the mediator 
+    public interface ICommand : ICommand<Unit>
+    {
+
+    }
+    public interface ICommand<out TResponse> : IRequest<TResponse>
+    {
+    }
+}
---
+++ src/BuildingBlocs/BuildingBlocs/CQRS/IQuery.cs
@@ -0,0 +1,10 @@
+﻿
+
+using MediatR;
+
+namespace BuildingBlocs.CQRS
+{
+    public  interface IQuery<out TResponse> : IRequest<TResponse> where TResponse : notnull
+    {
+    }
+}
