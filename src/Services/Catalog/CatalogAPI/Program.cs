8565a718bdd7e0fdf984cf8a6bf7bf01fb409e79
mohamed <mohammad.bushra@mensagroup.net>
2025-12-10 12:31:08 +0400

    end the first end point in the first MicroService

--- src/Services/Catalog/CatalogAPI/Program.cs
+++ src/Services/Catalog/CatalogAPI/Program.cs
@@ -1,9 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    //register all service into this project intpo mediator class library
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipline.
+app.MapCarter(); // with this line when initalize the project will scan all ICarter and map the endpoints 
 
 app.Run();
