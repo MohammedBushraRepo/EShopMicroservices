a301e253499e4159b44c7c644287b921755aefb6
Mohmaed <mohammad.bushra@mensagroup.net>
2026-02-25 12:08:51 +0400

    create products

--- src/Services/Catalog/CatalogAPI/Products/GetProducts/GetProductsEndPoint.cs
+++ src/Services/Catalog/CatalogAPI/Products/GetProducts/GetProductsEndPoint.cs
@@ -1,7 +1,26 @@
+
 namespace CatalogAPI.Products.GetProducts;
 
+//public record GetProductRequest();
+public record GetProductsResponse(IEnumerable<Product> Products);
 
-public class GetProductsEndPoint
+public class GetProductsEndPoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products", async (ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductsQuery());
 
+            var response = result.Adapt<GetProductsResponse>();
+
+            return Results.Ok(response);
+
+        })
+        .WithName("GetProducts")
+        .Produces<GetProductsResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Products")
+        .WithDescription("Get Products");
+    }
 }
